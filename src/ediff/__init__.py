5a8ecfa27a33c8ae3a4f14daf2776780e7629efc
mirekslouf <mirek.slouf@gmail.com>
2026-07-03 12:17:14 +0200

    V.1.2.7 - improved imports of bkg,bkg2d,gui

--- src/ediff/__init__.py
+++ src/ediff/__init__.py
@@ -21,37 +21,43 @@ EDIFF modules:
 * ediff.center = find the center of an arbitrary 2D-diffraction pattern
 * ediff.io = input/output operations (read diffractogram, set plot params...)
 * ediff.gcryst = functions from geometric/general crystallography
+* ediff.gui    = graphical user interface to ediff package
 * ediff.mcryst = process monocrystal diffraction patterns
 * ediff.pcryst = process polycrystal/powder diffraction patterns
 * ediff.radial = calculate the 1D-radial profile from a 2D-diffraction pattern
 '''
 
-__version__ = "1.2.6"
+__version__ = "1.2.7"
 
 
 # Import of modules so that we could use the package as follows:
 # >>> import ediff as ed
 # >>> ed.io.Diffractogram1D.show...
-from ediff import calibration
+from ediff import bkg
+from ediff import bkg2d
 from ediff import center
 from ediff import gcryst
+from ediff import gui
 from ediff import io
 from ediff import mcryst
 from ediff import pcryst
 from ediff import radial
 
-# This is a slightly special import:
-# (ediff (1) imports ediff.bkg, which (2) imports external bground package
-# (at the end, we can use all relevant bkg objects like: >>> ed.bkg.any_object
-from ediff import bkg
-
 # List of all ediff modules, which:
-# (enables >>> from ediff import *
-# (defines all sub-modules clearly and helps tools like VSCode/Pylance
+# 1) enables >>> from ediff import *
+# 2) defines all sub-modules clearly and helps tools like VSCode/Pylance
 __all__ = [
-    "bkg", "calibration", "center", "gcryst", "io", "pcryst", "mcryst", "radial"]
+    "bkg", "bkg2d", "calibration", "center", "gcryst",
+    "gui", "io", "pcryst", "mcryst", "radial"]
 
-# Obligatory acknowledgement -- the development was co-funded by TACR.
+# Technical notes - special imports
+# ----------------------------------
+# 1) ediff.bkg => imports external bground package
+# 2) ediff.bkg2d => under development - call funcs in auxiliary IDIFF package
+# 3) ediff.gui => graphical user interface - sub-package in its own subdir 
+
+# Obligatory acknowledgement -- the development was co-funded by TACR
+# -------------------------------------------------------------------
 #  TACR requires that the acknowledgement is printed when we run the program.
 #  Nevertheless, Python packages run within other programs, not directly.
 # The following code ensures that the acknowledgement is printed when:
@@ -59,7 +65,7 @@ __all__ = [
 #  (2) You run the package from command line: python -m ediff
 # Technical notes:
 #  To get item (2) above, we define __main__.py (next to __init__.py).
-#  The usage of __main__.py is not very common, but still quite standard.
+#  The usage of __main__.py is not too common, but still quite standard.
 
 def acknowledgement():
     print('EDIFF package - process electron diffraction patterns.')
